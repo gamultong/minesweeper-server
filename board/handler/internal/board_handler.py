95a580aecf2ff1ce4527d434ccdd80827a4838ec
onee-only <kimww0306@gmail.com>
2024-11-25 08:09:31 +0000

    BoardHandler new-conn receiver 추가

--- board/handler/internal/board_handler.py
+++ board/handler/internal/board_handler.py
@@ -1,13 +1,13 @@
 from event import EventBroker
-from board import Board
+from board import Board, Point
 from message import Message
-from message.payload import FetchTilesPayload, TilesPayload, TilesEvent
+from message.payload import FetchTilesPayload, TilesPayload, TilesEvent, NewConnEvent, NewConnPayload
 
 
 class BoardHandler():
     @EventBroker.add_receiver(TilesEvent.FETCH_TILES)
     @staticmethod
-    async def receive_fetch_tiles_event(message: Message[FetchTilesPayload]):
+    async def receive_fetch_tiles(message: Message[FetchTilesPayload]):
         tiles = Board.fetch(message.payload.start_p, message.payload.end_p)
 
         resp_message = Message(
@@ -17,6 +17,31 @@ class BoardHandler():
                 start_y=message.payload.start_y,
                 end_x=message.payload.end_x,
                 end_y=message.payload.end_y,
+                tiles=tiles
+            )
+        )
+
+        await EventBroker.publish(resp_message)
+
+    @EventBroker.add_receiver(NewConnEvent.NEW_CONN)
+    @staticmethod
+    async def receive_new_conn(message: Message[NewConnPayload]):
+        # 0, 0 기준으로 fetch
+        width = message.payload.width
+        height = message.payload.height
+        start_p = Point(x=-width, y=height)
+        end_p = Point(x=width, y=-height)
+
+        tiles = Board.fetch(start_p, end_p)
+
+        # TODO: header 추가하기. 위 메서드도
+        resp_message = Message(
+            event=TilesEvent.TILES,
+            payload=TilesPayload(
+                start_x=start_p.x,
+                start_y=start_p.y,
+                end_x=end_p.x,
+                end_y=end_p.y,
                 tiles=tiles
             )
         )
