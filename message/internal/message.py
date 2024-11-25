00192eda8f825929ff6904bae8b764466b9dcf6e
onee-only <kimww0306@gmail.com>
2024-11-25 11:07:08 +0000

    message header 추가

--- message/internal/message.py
+++ message/internal/message.py
@@ -16,13 +16,19 @@ PAYLOAD_DICT: dict[str, Payload] = {
 
 
 class Message(Generic[EVENT_TYPE]):
-    def __init__(self, event: str, payload: EVENT_TYPE):
+    def __init__(self, event: str, payload: EVENT_TYPE, header: dict[str, object] = {}):
         self.event = event
+        self.header = header
         self.payload = payload
 
-    def to_str(self):
+    def to_str(self, del_header: bool = True):
+        data = self
+        if del_header:
+            data = Message(event=self.event, payload=self.payload)
+            del data.header
+
         return json.dumps(
-            self,
+            data,
             default=lambda o: o.__dict__,
             sort_keys=True
         )
@@ -34,7 +40,11 @@ class Message(Generic[EVENT_TYPE]):
         event = decoded["event"]
         payload = decode_data(event, decoded["payload"])
 
-        return Message(event=event, payload=payload)
+        message = Message(event=event, payload=payload)
+        if "header" in decoded:
+            message.header = decoded["header"]
+
+        return message
 
 
 def decode_data(event: str, data: dict):
