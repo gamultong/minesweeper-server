0c1ed72daeba15140d737ba63260490792f26485
byundojin <byundojin0216@gmail.com>
2024-11-25 05:05:31 +0000

    new conn export

--- message/payload/internal/new_conn_payload.py
+++ message/payload/internal/new_conn_payload.py
@@ -9,28 +9,31 @@ class NewConnEvent(str, Enum):
     NEW_CONN = "new-conn"
     NEARYBY_CURSORS = "nearby-cursors"
     CURSOR_APPEARED = "cursor-appeared"
+    MY_CURSOR = "my-cursor"
 
 
 @dataclass
-class NewConnPayload():
+class NewConnPayload(Payload):
     conn_id: str
     width: int
     height: int
 
 
 @dataclass
-class CursorPayload():
-    position_x: int
-    position_y: int
-    point_x: int
-    point_y: int
+class CursorPayload(Payload):
+    position: Point
+    pointer: Point | None
     color: Color
 
 
 @dataclass
-class NearbyCursorPayload():
+class NearbyCursorPayload(Payload):
     cursors: list[CursorPayload]
 
 
 class CursorAppearedPayload(CursorPayload):
+    pass
+
+
+class NewCursorPayload(CursorPayload):
     pass
