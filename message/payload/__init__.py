d56ba7c53b90cca58158a1c30590ab733ad43265
byundojin <byundojin0216@gmail.com>
2024-11-25 06:28:41 +0000

    ParsablePayload 구현

--- message/payload/__init__.py
+++ message/payload/__init__.py
@@ -1,3 +1,4 @@
 from .internal.tiles_payload import FetchTilesPayload, TilesPayload, TilesEvent
 from .internal.base_payload import Payload
 from .internal.exceptions import InvalidFieldException, MissingFieldException
+from .internal.parsable_payload import ParsablePayload
