fdb6cb7c514305291d8056c05067f4a9afb0c140
byundojin <byundojin0216@gmail.com>
2024-11-26 03:03:57 +0000

    singleton mock 구현

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,23 +1,22 @@
-import unittest
-
-# message
-from message.test import *
-from message.payload.test import *
-
-# event
-from event.test import *
-
-# board
-from board.test import *
-from board.handler.test import *
-
-# conn
-from conn.test import *
-from conn.manager.test import *
-
-# cursor
-from cursor.test import *
-from cursor.manager.test import *
-
 if __name__ == "__main__":
+    import unittest
+    # message
+    from message.test import *
+    from message.payload.test import *
+
+    # event
+    from event.test import *
+
+    # board
+    from board.test import *
+    from board.handler.test import *
+
+    # conn
+    from conn.test import *
+    from conn.manager.test import *
+
+    # cursor
+    from cursor.test import *
+    from cursor.manager.test import *
+
     unittest.main()
--- tests/utils.py
+++ tests/utils.py
@@ -6,3 +6,39 @@ def cases(case_list):
                 func(*arg, **kwargs)
         return func_wrapper
     return wrapper
+
+
+def singleton_mock_setup(singleton_obj, mock, globals):
+    from pprint import pprint
+    origin_object = singleton_obj
+    globals[singleton_obj.__name__] = mock
+    return origin_object
+
+
+def singleton_mock_teardown(origin_object, globals):
+    globals[origin_object.__name__] = origin_object
+
+
+if __name__ == "__main__":
+    import unittest
+    from unittest.mock import MagicMock
+
+    class F:
+        pass
+
+    class SingletonMockTestCase(unittest.TestCase):
+        def setUp(self):
+            self.mock = MagicMock()
+            self.func_mock = MagicMock()
+            self.mock.func = self.func_mock
+
+            self.origin = singleton_mock_setup(F, self.mock, globals())
+
+        def tearDown(self):
+            singleton_mock_teardown(self.origin, globals())
+
+        def test_case(self):
+            F.func()
+            self.assertEqual(len(self.func_mock.mock_calls), 1)
+
+    unittest.main()
