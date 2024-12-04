5936055ce6b6c3d9712d28996e273293eb6520a4
byundojin <byundojin0216@gmail.com>
2024-12-04 01:18:28 +0000

    export 및 event enum 추가

--- message/payload/internal/interaction_payload.py
+++ message/payload/internal/interaction_payload.py
@@ -2,20 +2,27 @@ from .base_payload import Payload
 from .parsable_payload import ParsablePayload
 from board.data import Point, Tile
 from dataclasses import dataclass
+from enum import Enum
+
+
+class InteractionEvent(Enum):
+    YOU_DIED = "you-died"
+    TILE_UPDATED = "tile-updated"
+    TILE_STATE_CHANGED = "tile-state-changed"
 
 
 @dataclass
-class YouDiedPayload():
+class YouDiedPayload(Payload):
     revive_at: str
 
 
 @dataclass
-class TileUpdatedPayload():
+class TileUpdatedPayload(Payload):
     position: ParsablePayload[Point]
     tile: ParsablePayload[Tile]
 
 
 @dataclass
-class TileStateChangedPayload():
+class TileStateChangedPayload(Payload):
     position: ParsablePayload[Point]
     tile: ParsablePayload[Tile]
